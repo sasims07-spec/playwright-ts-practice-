0d0c5b5ab306a78e38c60a609987e5972a68a530
Sasirekha Jeganathan <sasirekha.jeganathan@sscinc.com>
2026-07-08 21:16:28 +0100

    Added playwright yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   //workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? "50%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { outputFolder: "reports/my-html-report", open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 60000, // single test
   expect: {
